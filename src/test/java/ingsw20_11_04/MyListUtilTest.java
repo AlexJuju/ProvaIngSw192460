a6760717c41ef94f55ff1e7f86cc4cd70ff921e5
AlexJuju <account4s@live.it>
2020-11-04 12:39:30 +0100

    added second tests on both testing methods

--- src/test/java/ingsw20_11_04/MyListUtilTest.java
+++ src/test/java/ingsw20_11_04/MyListUtilTest.java
@@ -12,20 +12,24 @@ public class MyListUtilTest {
 
 	public static MyListUtil mylist;
 	public static ArrayList <Integer> list;
+	public static ArrayList <Integer> list1;
 	
 	@BeforeClass
 	public static void init () {
 		mylist = new MyListUtil();
 		list = new ArrayList<Integer> (Arrays.asList(5,3,6,9,2,5,8));
+		list1 = new ArrayList<Integer> ();
 	}
 	
 	@Test
 	public void TestOrdAscendente () {
 		assertEquals( Arrays.asList(2,3,5,5,6,8,9), mylist.sortUp( list ) );
+		assertEquals( new ArrayList<Integer> (), mylist.sortUp(list1) );
 	}
 	
 	@Test
 	public void TestOrdDiscendente () {
 		assertEquals( Arrays.asList(9,8,6,5,5,3,2), mylist.sortDown( list ) );
+		assertEquals( new ArrayList<Integer> (), mylist.sortDown(list1) );
 	}
 }
